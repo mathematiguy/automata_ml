2d04ff321ed34ce699e6533aa5d46bf85591d291
Caleb Moses <calebjdmoses@gmail.com>
2021-08-31 21:56:56 +1200

    Update figures

--- notebooks/figures/gen_figs.sh
+++ notebooks/figures/gen_figs.sh
@@ -2,7 +2,7 @@
 
 base="notebooks/figures"
 
-python figures/simple_automata.py
+python3 ${base}/simple_automata.py
 
 for f in 'simple_automata' 'simple_dfa' 'simple_nfa'
 do
@@ -11,12 +11,12 @@ do
     sed -i '' 's/c\/0/c/g' ${base}/$f.svg
 done
 
-python figures/epsilon_intersect.py
+python3 ${base}/epsilon_intersect.py
 
 ef=epsilon_intersect_both
 for f in "_1" "_2" "" "_correct"
 do
-    dot -Tsvg figures/${ef}${f}.dot -o ${base}/${ef}${f}.svg
+    dot -Tsvg ${base}/${ef}${f}.dot -o ${base}/${ef}${f}.svg
 done
 
 for i in {1..10}
@@ -24,12 +24,12 @@ do
     for g in 'g1' 'g2' 'g_out'
     do
         gf=intersect_step_${i}_${g}
-        dot -Tsvg figures/intersect_steps/${gf}.dot -o ${base}/${gf}.svg
+        dot -Tsvg ${base}/intersect_steps/${gf}.dot -o ${base}/${gf}.svg
     done
 done
 
 for f in 'epsilon_intersect' 'viterbi_recursion' 'viterbi_path_back' \
          'unigram' 'bigram' 'asg_bigrams'
 do
-    dot -Tsvg figures/${f}.dot -o ${base}/${f}.svg
+    dot -Tsvg ${base}/${f}.dot -o ${base}/${f}.svg
 done
